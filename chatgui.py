516bb4e67d0a7ffea71708a994eb9d092f221d94
masteramuk <masteramuk@yahoo.com>
2020-06-05 15:44:01 +0800

    change some of the import command as original import does not work

--- chatgui.py
+++ chatgui.py
@@ -4,8 +4,13 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
+import tensorflow as tf
 
-from keras.models import load_model
+from tensorflow.python import keras
+from tensorflow.python.keras.models import load_model
+#from tensorflow_core.python.keras.api import keras
+#from keras.model import load_model
+
 model = load_model('chatbot_model.h5')
 import json
 import random
@@ -75,22 +80,22 @@ def send():
     if msg != '':
         ChatLog.config(state=NORMAL)
         ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+        ChatLog.config(foreground="#CCCC00", font=("Verdana", 12 ))
 
         res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
+        ChatLog.insert(END, "Bot: " + res + '\n')
 
         ChatLog.config(state=DISABLED)
         ChatLog.yview(END)
 
 
 base = Tk()
-base.title("Hello")
+base.title("Chatbot")
 base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
 #Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog = Text(base, bd=0, bg="grey", height="8", width="50", font="Arial",)
 
 ChatLog.config(state=DISABLED)
 
@@ -100,11 +105,11 @@ ChatLog['yscrollcommand'] = scrollbar.set
 
 #Create Button to send message
 SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    bd=0, bg="#000000", activebackground="#dddd90",fg='#ffffff',
                     command= send )
 
 #Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox = Text(base, bd=0, bg="grey",width="29", height="5", font="Arial")
 #EntryBox.bind("<Return>", send)
 
 
